af4b04c35e95a93946c3b282b0ff561a3ffb89db
Destan Dicle Özgen <v566wsq8j2@privaterelay.appleid.com>
2026-07-20 13:40:10 +0300

    Update IStudentService.cs

--- University.Business/Abstract/IStudentService.cs
+++ University.Business/Abstract/IStudentService.cs
@@ -6,5 +6,9 @@ namespace University.Business.Abstract
     public interface IStudentService
     {
         List<StudentDto> GetAllStudents();
+        StudentDto GetStudentById(int id);
+        StudentDto CreateStudent(CreateStudentDto dto);
+        StudentDto UpdateStudent(int id, CreateStudentDto dto);
+        bool DeleteStudent(int id);
     }
 }
