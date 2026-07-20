1e6a6b578f83d9b30a5ec117d53a745d0036b624
Destan Dicle Özgen <v566wsq8j2@privaterelay.appleid.com>
2026-07-20 13:38:42 +0300

    Update StudentDto.cs

--- University.Business/DTOs/StudentDto.cs
+++ University.Business/DTOs/StudentDto.cs
@@ -5,5 +5,14 @@ namespace University.Business.DTOs
         public int Id { get; set; }
         public string FullName { get; set; }
         public string StudentNumber { get; set; }
+        public string DepartmentName { get; set; }
+    }
+
+    public class CreateStudentDto
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string StudentNumber { get; set; }
+        public int DepartmentId { get; set; }
     }
 }
